d78135a931332d4de75f814f8e05121550ff98fd
KaptnDi <ddobler@stud.hs-heilbronn.de>
2017-05-08 11:42:45 +0200

    funzt

---
+++ flash/src/flashcards/FlashcardTest.java
@@ -0,0 +1,25 @@
+package flashcards;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dimitri on 08.05.2017.
+ */
+public class FlashcardTest {
+    @org.junit.Test
+    public void getTerm() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setTerm() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getDescription() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setDescription() throws Exception {
+    }
+
+}
